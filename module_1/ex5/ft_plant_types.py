fb95e3438a60531ec3fbf4a27ed8eea6bc8fbaeb
Rafael <136873880+RegulusRK@users.noreply.github.com>
2026-09-01 23:24:22 -0300

    feat: progress module 1 ex6

--- module_1/ex5/ft_plant_types.py
+++ module_1/ex5/ft_plant_types.py
@@ -39,36 +39,80 @@ class Plant:
             f"{self.name}: {round(self._height, 1)}cm, {self._days} days old"
             )
 
+
 class Flower(Plant):
-    def __init__(self, name: str, _height: float, _days: int, growth: float, color: str, bloomed: bool) -> None:
-        super().__init__(name, _height, _days, growth)
+    def __init__(self, name: str, height: float, days: int,
+                 growth: float, color: str) -> None:
+        super().__init__(name, height, days, growth)
         self.color = color
         self.bloomed = False
-        self.bloomed = bloomed
 
     def bloom(self) -> None:
-        if (self.bloomed == False):
-            print(f'{self.name}: has not bloomed yet')
-            print('[asking the rose to bloom]')
-            self.bloomed = True
-        elif (self.bloomed == True):
-            self.show()
+        self.bloomed = True
+
+    def show(self) -> None:
+        super().show()
+        print(f'Color: {self.color}')
+        if (self.bloomed is True):
             print(f'{self.name} is blooming beautifully!')
+        elif (self.bloomed is False):
+            print(f'{self.name} has not bloomed yet')
+
 
 class Tree(Plant):
-    def __init__(self, name: str, _height: float, _days: int, growth: float, trunk_diameter: float) -> None:
-        super().__init__(name, _height, _days, growth)
+    def __init__(self, name: str, height: float, days: int, growth: float,
+                 trunk_diameter: float) -> None:
+        super().__init__(name, height, days, growth)
         self.trunk_diameter = trunk_diameter
 
+    def show(self) -> None:
+        super().show()
+        print(f'Trunk diameter: {self.trunk_diameter}cm')
+
     def produce_shade(self) -> None:
-        print('[asking the oak to produce shade]')
-        print(f'Tree Oak now produces a shade of {self._height} long and {self.trunk_diameter} wide.')
+        print(
+              f'Tree {self.name} now produces a shade of '
+              f'{self._height}cm long and {self.trunk_diameter}cm wide.'
+        )
+
 
 class Vegetable(Plant):
-    def __init__(self, name: str, _height: float, _days: int, growth: float, harvest_season, nutritional_value) -> None:
-        super().__init__(name, _height, _days, growth)
+    def __init__(self, name: str, height: float, days: int, growth: float,
+                 harvest_season: str) -> None:
+        super().__init__(name, height, days, growth)
         self.harvest_season = harvest_season
-        self.nutritional_value = nutritional_value
+        self.nutritional_value = 0
+
+    def show(self) -> None:
+        super().show()
+        print(f'Harvest season: {self.harvest_season}')
+        print(f'Nutritional value: {self.nutritional_value}')
+
+    def age(self) -> None:
+        super().age()
+        self.nutritional_value += 1
 
 
 if __name__ == "__main__":
+    print('=== Garden Plant Types ===')
+    print('=== Flower')
+    rose = Flower('Rose', 15.0, 10, 0, 'red')
+    rose.show()
+    print('[asking the rose to bloom]')
+    rose.bloom()
+    rose.show()
+    print('\n')
+    print('=== Tree')
+    oak = Tree('Oak', 200.0, 365, 0, 5.0)
+    oak.show()
+    print('[asking the oak to produce shade]')
+    oak.produce_shade()
+    print('\n')
+    print('=== Vegetable')
+    tomato = Vegetable('Tomato', 5.0, 10, 2.10, 'April')
+    tomato.show()
+    print('[make tomato grow and age for 20 days]')
+    for _ in range(0, 20):
+        tomato.age()
+        tomato.grow()
+    tomato.show()
